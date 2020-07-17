42655d3427480663c38f859a968f67eba8449214
Aditya Shekhar Tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-07-17 21:00:18 +0530

    Add files via upload

---
+++ greates of three no.s.py
@@ -0,0 +1,11 @@
+x=int(input("enter the no."))
+y=int(input("enter the no."))
+z=int(input("enter the no."))
+if x>y:
+    max=x
+else:
+   max=y
+if max>z:
+    print(max)
+else :
+    print(z)
---
+++ perfect sq. no.py
@@ -0,0 +1,6 @@
+for i in range(1,51):
+	for x in range(1,8):
+		if i/x==x:
+			print(i)
+	i+=1
+	print()
