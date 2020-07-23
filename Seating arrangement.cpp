d8ac62744f175898998e72603f99673c7ce3eaf4
Aditya Shekhar Tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-07-23 16:30:25 +0530

    Add files via upload
    So they got interested to know the seat number facing them and the seat type facing them. The seats are denoted as follows :
    
    Window Seat : WS
    Middle Seat : MS
    Aisle Seat : AS
    
    You will be given a seat number, find out the seat number facing you and the seat type, i.e. WS, MS or AS.
    
    INPUT
    First line of input will consist of a single integer T denoting number of test-cases. Each test-case consists of a single integer N denoting the seat-number.
    
    OUTPUT
    For each test case, print the facing seat-number and the seat-type, separated by a single space in a new line.
    
    CONSTRAINTS
    1<=T<=105
    1<=N<=108

---
+++ Seating arrangement.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int b,a,i;
+    //b is no. of input
+    
+    cin>> b;
+    
+    
+    for(i=0;i<b;i++){
+   
+    cin >> a;
+    
+    if(a%6==0){
+        cout<< a+1 <<" " <<"WS\n";}
+    
+    else if(a%6==1){
+        cout<< a+11 <<" " <<"WS\n";}
+    
+    else if(a%6==2){
+        cout<< a+9 <<" " <<"MS\n";}
+        
+    else if(a%6==3){
+        cout << a+7 <<" " <<"AS\n";}
+        
+    else if(a%6==4){
+        cout<< a+5 << " " <<"AS\n";}
+        
+    else if(a%6==5){
+        cout<< a+3<< " "<<"MS\n";}
+    
+}
+}
