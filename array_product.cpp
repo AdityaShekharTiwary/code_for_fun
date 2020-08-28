d6a93d3f2ff46f0c8826c7acada312c7cea10a4f
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-08-28 11:34:24 +0530

    Add files via upload

---
+++ array_product.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int productAllNumber(int N) {
+    long long int ans = 1;
+    const int C = 1000000007;
+    int num;
+    for (int i = 0; i < N; i++) {
+        cin >> num;
+        ans = (ans*num) % C;
+    }
+    return ans;
+}
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    cout << productAllNumber(N) << endl;
+
+    return 0;
+}
