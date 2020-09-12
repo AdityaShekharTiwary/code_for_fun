6e2d18ed0f200ffb5e8116f4d97f4924e7db3b85
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-12 20:39:22 +0530

    Pass By Refrence

---
+++ pass_by_refrence.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void passByValue(int x,int y)
+{
+	int z = x;
+	x=y;
+	y=z;
+}
+void passByRefrence(int &x,int &y)
+{
+	int z=x;
+	x=y;
+	y=z;
+}
+
+int main()
+{
+	int a=5,b=6;
+	cout << "before swapping " << endl << "a: " << a << endl << "b: " << b << endl << endl;
+	// call function
+	passByRefrence(a,b);
+	
+	cout <<"After swapping " << endl <<"a: " << a << endl <<"b: " << b <<endl <<endl;
+	return 0;
+}
