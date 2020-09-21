1831637f1c3b961b8ce8f4f8d0f6e37fccf45b0f
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2020-09-21 16:02:30 +0530

    JavaScript  Basic Syntax

---
+++ Hello.js
@@ -0,0 +1,15 @@
+console.log('Hello World');
+
+var fullname = "Aditya Tiwary";
+
+var courseNaem = "React JS Bootcamp";
+
+var islogged = false ;
+
+var loggedCount = 34;
+
+var paymentMode;
+paymeentMode="Credit Card";
+
+console.log(loggedCount)
+console.log("loggedCount");
